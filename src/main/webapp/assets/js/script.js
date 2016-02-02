442dce9e522127bdf4329ebf92db9d9674186612
vboar <vboar@live.com>
2016-02-02 23:41:00 +0800

    toaster

--- src/main/webapp/assets/js/script.js
+++ src/main/webapp/assets/js/script.js
@@ -4,8 +4,14 @@ $(document).ready(function () {
 
 function toaster(message, type) {
     var toaster = $("#toaster");
-    toaster.append('<div class="toast-item"><div class="message">' + message + '</div></div>');
+    toaster.append('<div class="toast-item"><div class="message">' + message + '</div>' +
+        '<i class="close fa fa-close"></i></div>');
     var thisItem = toaster.children().last();
+    $(thisItem.children(".close").eq(0)).bind("click", function () {
+        thisItem.slideUp(function() {
+            thisItem.remove();
+        });
+    });
     if (type == "success") thisItem.addClass("success");
     else if (type == "error") thisItem.addClass("error");
     thisItem.fadeIn();
