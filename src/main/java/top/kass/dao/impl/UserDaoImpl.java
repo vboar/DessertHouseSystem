a4c22f3071e66b703b88523c86e37ad4c096a9ec
vboar <vboar@live.com>
2016-02-24 10:37:39 +0800

    test (reverted from commit 28cf1597c34248ee109f0b1f851685fc67136b87)

---
+++ src/main/java/top/kass/dao/impl/UserDaoImpl.java
@@ -0,0 +1,59 @@
+package top.kass.dao.impl;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import top.kass.dao.UserDao;
+import top.kass.model.*;
+
+import javax.transaction.Transactional;
+import java.sql.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+@Repository
+@Transactional
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public User test() {
+        Session session = sessionFactory.getCurrentSession();
+        User user = (User)session.get(User.class, 3);
+        return user;
+    }
+
+    @Override
+    public void test1() {
+        Session session = sessionFactory.getCurrentSession();
+
+    }
+
+    @Override
+    public UserShop test2() {
+        Session session = sessionFactory.getCurrentSession();
+        UserShop userShop = (UserShop)session.get(UserShop.class, 3);
+        return userShop;
+    }
+
+    @Override
+    public Shop test3() {
+        Session session = sessionFactory.getCurrentSession();
+        Shop shop = (Shop)session.get(Shop.class, 1);
+        Set<UserShop> userShops = shop.getUserShops();
+        Iterator<UserShop> it = userShops.iterator();
+        return shop;
+    }
+
+    @Override
+    public Customer test4() {
+        Session session = sessionFactory.getCurrentSession();
+        Customer customer = (Customer)session.get(Customer.class, 1);
+        return customer;
+    }
+
+
+}
