d1ff45f7347bc5789d2bbab75de4f5942eae4238
vboar <vboar@live.com>
2016-02-24 10:36:17 +0800

    test

--- src/main/java/top/kass/dao/impl/CustomerDaoImpl.java
+++ src/main/java/top/kass/dao/impl/CustomerDaoImpl.java
@@ -3,7 +3,6 @@ package top.kass.dao.impl;
 import org.springframework.stereotype.Repository;
 import top.kass.dao.CustomerDao;
 import top.kass.model.Customer;
-import top.kass.util.Utils;
 
 @Repository
 public class CustomerDaoImpl implements CustomerDao {
